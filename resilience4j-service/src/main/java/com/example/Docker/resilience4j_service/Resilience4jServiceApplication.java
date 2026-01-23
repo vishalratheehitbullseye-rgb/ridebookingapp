8525f3ffbe48c0fc79cf70da95196bd802e0883d
vishalratheehitbullseye-rgb <vishalrathee.hitbullseye@gmail.com>
2026-01-23 12:20:00 +0530

    Resilience 4j set up

---
+++ resilience4j-service/src/main/java/com/example/Docker/resilience4j_service/Resilience4jServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Docker.resilience4j_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jServiceApplication.class, args);
+	}
+
+}
---
+++ resilience4j-service/src/test/java/com/example/Docker/resilience4j_service/Resilience4jServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Docker.resilience4j_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
